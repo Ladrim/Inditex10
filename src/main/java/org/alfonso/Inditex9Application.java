7fe4abfc19b7f788ea677e0a5939e9b2c9a391c5
ladri <ladrin@gmail.com>
2025-08-15 09:52:09 +0200

    16

--- src/main/java/org/alfonso/Inditex9Application.java
+++ src/main/java/org/alfonso/Inditex9Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Inditex9Application
 {
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		SpringApplication.run(Inditex9Application.class, args);
 	}
 
